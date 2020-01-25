9fbe69aa1765518550c76862f11c1a3c6e37584f
imtiaz <imtiaz@adzymic.co>
2020-01-25 22:18:27 +0800

    user login without serving JWT token

--- routes/api/users.js
+++ routes/api/users.js
@@ -48,4 +48,28 @@ router.post("/register", (req, res) => {
   });
 });
 
+/*
+@route   POST /api/users/login
+@desc    Login User / Return JWT - Token
+@access  Public
+*/
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        return res.json({ msg: "Login Successful" });
+      } else {
+        return res.status(400).json({ password: "Password Incorrect" });
+      }
+    });
+  });
+});
+
 module.exports = router;
