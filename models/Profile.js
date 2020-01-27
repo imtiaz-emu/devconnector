186059967db1ca733b6906bc3b7575ac38153db0
imtiaz <imtiaz@adzymic.co>
2020-01-27 20:34:00 +0800

    Added profile model

---
+++ models/Profile.js
@@ -0,0 +1,104 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ProfileSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users"
+  },
+  company: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  github: {
+    type: String
+  },
+  skills: [String],
+  experience: [
+    {
+      company: {
+        type: String,
+        required: true
+      },
+      title: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  education: [
+    {
+      institute: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    }
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
