a74d33b0f475cf9f005e2fbab738d6b6322cba2d
imtiaz <imtiaz@adzymic.co>
2020-02-01 18:12:31 +0800

    get current user profile

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+
+// Load Models
+const Profile = require("../../models/Profile");
 
 /*
 @route   GET /api/profiles/test
@@ -7,5 +11,27 @@ const router = express.Router();
 @access  Public
 */
 router.get("/test", (req, res) => res.json({ msg: "Profile Working" }));
+
+/*
+@route   GET /api/profiles
+@desc    Get current user profile
+@access  Private
+*/
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
